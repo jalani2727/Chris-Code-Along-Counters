0158823b3a21be3827bcd24e5b7b3438d585cb04
Jalani Paul <jalani2727@gmail.com>
2018-07-17 15:10:53 -0400

    added click function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from 'react'; //this is like using require to pull in the React library
 import Counter from "./Counter.js"; // the variable name isn't really referring to the variable name in the file you're importing from. It's just making a label for use in this file. The vairable name in this file doesnt have to match the variable name of what's being exported in the file that's being imported here
 
+const handleClick = () => {
+  console.log("hay")
+}
+
 
 const App = () => {
 
@@ -8,15 +12,30 @@ const App = () => {
 
   return ( 
   <div className = "container">
-    <button> + </button>
+    <button onClick={handleClick}> + </button>
     <div className = "counter-box">
     {/* here are 5 function calls */}
     {/* it looks like you;re giving a class to some html. but what's actually happening is you;re assigning a value that's picked up becuase it's being listened for in the definition of the counter function in the Counter.js file */}
-    <Counter finalValue = "100" initialValue="42"/>
-    <Counter finalValue = "100" initialValue="3"/>
-    <Counter finalValue = "100" initialValue="18"/>
-    <Counter finalValue = "100" initialValue="56"/>
-    <Counter finalValue = "100" initialValue="29"/>
+    <Counter 
+    finalValue = "100" 
+    initialValue="42"
+    />
+    <Counter 
+    finalValue = "100" 
+    initialValue="3"
+    />
+    <Counter 
+    finalValue = "100" 
+    initialValue="18"
+    />
+    <Counter 
+    finalValue = "100" 
+    initialValue="56"
+    />
+    <Counter 
+    finalValue = "100" 
+    initialValue="29"
+    />
 
 
     </div>
--- src/Counter.js
+++ src/Counter.js
@@ -1,4 +1,14 @@
 import React from 'react'; 
+// const Counter = (props) => {
+//     console.log(props/*"properties"*/);
+//     return(
+//         <div className = "counter">
+//         {props.initialValue}
+//         </div>
+//     );
+// };
+
+// w/ object destructuring. adds the values you're looking for as arguments in place of props as the argument
 const Counter = ({finalValue, initialValue}) => {
     // console.log(props/*"properties"*/);
     return(
