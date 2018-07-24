c91b371a3adff5cf2e8efc5e781fa2598093236d
Jalani Paul <jalani2727@gmail.com>
2018-07-24 15:09:54 -0400

    mapped the notes array into a title array in the documentlist component

--- notes-app/src/App.js
+++ notes-app/src/App.js
@@ -5,13 +5,36 @@ import SearchBar from './SearchBar';
 import DocumentEditor from './DocumentEditor';
 
 
+
 class App extends React.Component {
+
+constructor(props) {
+  super(props);
+
+  this.state = {
+    notes: [
+      {
+        id:10001,
+        timestamp: new Date(),
+        title: 'Woo!: A Novel',
+        content:"WOO! - Ric Flair"
+      },
+      {
+        id:10002,
+        timestamp: new Date(),
+        title: 'Woo! 2: A Sequel to "Woo!: A Novel"',
+        content:'"woo!" - Ric Flair Jr.' 
+      }
+    ]
+  }
+}
+
   render() {
     return (
       <div className="notes-app">
         
         <SearchBar/> 
-        <DocumentList/>
+        <DocumentList allNotes={this.state.notes} />
 
         <div className="document-editor">
         <DocumentEditor/> 
--- notes-app/src/DocumentList.js
+++ notes-app/src/DocumentList.js
@@ -4,15 +4,31 @@ import React, {Component} from 'react';
 
 class DocumentList extends Component{
     render() {
+        // What is happening here?
+        // that allNotes property is an array. The map tacked noto the end is turning that array into something else. The parentheses are the instructors for how to tranform the array the way you want. The instructions are a function. The function is written with an implicit return but it doesnt have to be written this way.
+        let items = this.props.allNotes.map(note =><li className="document-list-item">{note.title}</li>
+        )
+
         return (
             <div className="document-list">
             <ul>
-            <li  className="document-list-item"> Note One</li>
-            <li  className="document-list-item"> Note Two</li>
-            <li  className="document-list-item"> Note Three</li>
+                {items}
             </ul>
             </div>
-        )
+        )}
 }
-}
+
+// A function component is like a
+// "render-only" component.
+// It can't have state.
+// It doesn't have lifecycle methods like
+// componentDidMount or constructor
+// const DocumentEditor = (props) => {
+//   return (
+//     <div className="document-editor">
+//       <textarea value="This is a note. See?" />
+//     </div>
+//   )
+// }
+
  export default DocumentList;
