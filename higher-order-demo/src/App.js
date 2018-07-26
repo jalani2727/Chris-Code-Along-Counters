dc1f06f7e6ab78948bec9514c044391e81a1d538
Jalani Paul <jalani2727@gmail.com>
2018-07-26 10:41:08 -0400

    better way to use higher-order functions in comments

--- higher-order-demo/src/App.js
+++ higher-order-demo/src/App.js
@@ -17,26 +17,44 @@ class App extends Component {
 }
 
 
-
 const RenderOther = (props) => {
   const OtherComponent = props.other;
   return(
     <div>
       <h1>I am RenderOther!</h1>
-      <OtherComponent {...props}/> {/* OtherComponent is able to recognize RenderMe. It is the bridge between RenderOther and RenderMe thanks to {...props}*/}
+      <OtherComponent {...props}/> {/* OtherComponent is able to recognize RenderMe. It is the bridge between RenderOther and RenderMe thanks to {...props} */}
     </div>
   )
 };
-
+// Other way. Better Way. Real Way. Useful Way.
+// const RenderOtherToo = ({other: OtherComponent, ...rest}) => {
+//   return (
+//     <div>
+//       <h1>I too am Render Other!!</h1>
+//       <OtherComponent {...rest} />
+//     </div>
+//   );
+// }
 
 
 const RenderMe = (props) =>{
   return(
     <ul>
-      <li>I am RenderMe. I am being passed to RenderOther as a property. I can be seen by RenderOther via the ...props thing in OtherComponent </li>
+      <li>I am RenderMe. I am being passed to RenderOther as a property. Normally, I wouldn't be seen. However, I can be seen by RenderOther via the ...props thing in OtherComponent which is a component of RenderOther. </li>
+      <br/><br/>
       <li>{props.text}</li>
     </ul>
   )
 }
+// Other way. Better Way. Real Way. Useful Way.
+// class RenderMeToo extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <h2>I am RenderMe. I am being passed to RenderOther as a property. Normally, I wouldn't be seen. However, I can be seen by RenderOther via the ...props thing in OtherComponent which is a component of RenderOther.</h2>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
